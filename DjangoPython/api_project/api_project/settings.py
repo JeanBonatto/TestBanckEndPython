ef40d1a862b0cf5d95dc02e102fd76c0cda84e2d
JeanBonatto <jb.bonatto@outlook.com>
2023-05-30 23:37:44 -0300

    Desenvolvimento API

--- DjangoPython/api_project/api_project/settings.py
+++ DjangoPython/api_project/api_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'devices'
 ]
 
 MIDDLEWARE = [
@@ -76,7 +78,11 @@ WSGI_APPLICATION = 'api_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'SoftmusServer'
+        'USER': 'postgre',
+        'PASSWORD': 'jean1234',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
--- DjangoPython/api_project/devices/models.py
+++ DjangoPython/api_project/devices/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
 # Create your models here.
+class Device(models.Model):
+    name = models.CharField(max_length=100)
+    mac_address = models.CharField(max_length=17)
+
+    def __str__(self):
+        return self.name
+
+class DataEntry(models.Model):
+    value = models.FloatField()
+    date_time = models.DateTimeField()
+    device = models.ForeignKey(Device, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.value)
